6574ae48ba50c514da03c13d75096adf3af7ee9c
Leonardo Augusto <leonardoaugusto.soliveira@gmail.com>
2024-06-08 23:03:04 -0300

    feat:frontend: Initial

---
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignIn() {
+  return (
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">password</Label>
+        <Input name="password" type="password" id="password" />
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-xs font-medium text-foreground hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+
+      <Button type="submit" className="w-full">
+        Sign in with e-mail
+      </Button>
+
+      <Button variant="link" className="w-full" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" variant="outline" className="w-full">
+        <Image src={githubIcon} className="mr-2 size-4 dark:invert" alt="" />
+        Sign in with Github
+      </Button>
+    </form>
+  )
+}
